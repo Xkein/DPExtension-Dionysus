2ca59760932c5b8115eea34f8e1510f7186519ff
Xkein <739502808@qq.com>
2021-09-07 09:07:50 +0800

    add Preprocess.

---
+++ Miscellaneous/Preprocess.cs
@@ -0,0 +1,20 @@
+﻿using DynamicPatcher;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miscellaneous
+{
+    [RunClassConstructorFirst]
+    public class Preprocess
+    {
+        static Preprocess()
+        {
+            // add 500MB pressure
+            GC.AddMemoryPressure(500 * 1024 * 1024);
+            Logger.Log("Add 500MB pressure to GC.");
+        }
+    }
+}
