6e2fa72a4d9d174db1dab42ed1134ed792e0b5ff
Xkein <739502808@qq.com>
2022-03-26 20:32:08 +0800

    create lua project

---
+++ Projects/Extension.Lua/LuaScriptFinder.cs
@@ -0,0 +1,23 @@
+﻿using NLua;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension.lua
+{
+    public static class LuaScriptFinder
+    {
+        public const string LUA_DIRECTORY_NAME = "lua";
+        public static string LuaSearchDirectory = Path.Combine(Directory.GetCurrentDirectory(), LUA_DIRECTORY_NAME);
+
+        public static Lua Find(string luaScriptName)
+        {
+
+            return null;
+        }
+
+    }
+}
