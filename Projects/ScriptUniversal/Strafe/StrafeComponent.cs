8b7f33facd0fa4b42826f0ea2b6acf3770238380
Xkein <739502808@qq.com>
2022-05-21 21:34:39 +0800

    add ScriptUniversal project

---
+++ Projects/ScriptUniversal/Strafe/StrafeComponent.cs
@@ -0,0 +1,24 @@
+﻿using Extension.Components;
+using PatcherYRpp;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Extension.Coroutines;
+using ScriptUniversal.Strategy;
+
+namespace ScriptUniversal.Strafe
+{
+    [Serializable]
+    public abstract class StrafeComponent : WeaponFireStrategy
+    {
+        public StrafeComponent(Pointer<TechnoClass> techno, Pointer<WeaponTypeClass> weapon) : base(techno, weapon)
+        {
+
+        }
+
+
+    }
+}
