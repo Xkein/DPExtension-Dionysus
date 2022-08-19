d87baa8d90bc25fca4dd8e5102622c80dc0b7bf1
Xkein <739502808@qq.com>
2022-08-19 19:35:01 +0800

    add SuperWeaponRecorder script

---
+++ Scripts/SuperWeaponRecorder.cs
@@ -0,0 +1,37 @@
+﻿using DynamicPatcher;
+using Extension.Ext;
+using Extension.Script;
+using PatcherYRpp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scripts
+{
+    // !!!!!!!!!!!!!!!!!!!!
+    // you can use GlobalScriptable everywhere unless your assembly outside DynamicPatcher folder
+
+
+    [GlobalScriptable(typeof(SuperWeaponExt))]
+    public class SuperWeaponRecorder : SuperWeaponScriptable
+    {
+        public SuperWeaponRecorder(SuperWeaponExt owner) : base(owner)
+        {
+        }
+
+        public override void Awake()
+        {
+            Logger.Log("{0} start tick.", Owner.OwnerTypeRef.Base.ID);
+        }
+        public override void OnDestroy()
+        {
+            Logger.Log("{0} end tick", Owner.OwnerTypeRef.Base.ID);
+        }
+        public override void OnLaunch(CellStruct cell, bool isPlayer)
+        {
+            Logger.Log("{0} fire at ({1}, {2})", Owner.OwnerTypeRef.Base.ID, cell.X, cell.Y);
+        }
+    }
+}
