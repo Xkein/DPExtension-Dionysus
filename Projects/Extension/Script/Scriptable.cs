a9f6ad07b60150ca60e9942b480fc85b35e335fe
Xkein <739502808@qq.com>
2022-08-19 19:20:06 +0800

    move things to Extension.Script.csproj

--- Projects/Extension/Script/Scriptable.cs
+++ Projects/Extension/Script/Scriptable.cs
@@ -10,6 +10,21 @@ using System.Threading.Tasks;
 
 namespace Extension.Script
 {
+    [AttributeUsage(AttributeTargets.Class, Inherited = true)]
+    public sealed class GlobalScriptableAttribute : Attribute
+    {
+        public GlobalScriptableAttribute(params Type[] types)
+        {
+            Types = types;
+        }
+
+        public Type[] Types { get; }
+    }
+
+    public interface IScriptable : IReloadable
+    {
+    }
+
     [Serializable]
     public abstract class Scriptable<T> : ScriptComponent, IScriptable
     {
@@ -21,92 +36,4 @@ namespace Extension.Script
         }
     }
 
-    [Serializable]
-    public class TechnoScriptable : Scriptable<TechnoExt>, ITechnoScriptable
-    {
-        public TechnoScriptable(TechnoExt owner) : base(owner)
-        {
-        }
-
-        public virtual void OnPut(CoordStruct coord, Direction faceDir) { }
-        public virtual void OnRemove() { }
-        public virtual void OnReceiveDamage(Pointer<int> pDamage, int DistanceFromEpicenter, Pointer<WarheadTypeClass> pWH,
-            Pointer<ObjectClass> pAttacker, bool IgnoreDefenses, bool PreventPassengerEscape, Pointer<HouseClass> pAttackingHouse)
-        { }
-
-        public virtual void OnFire(Pointer<AbstractClass> pTarget, int weaponIndex) { }
-    }
-
-    [Serializable]
-    public class BulletScriptable : Scriptable<BulletExt>, IBulletScriptable
-    {
-        public BulletScriptable(BulletExt owner) : base(owner)
-        {
-        }
-
-        [Obsolete("not support OnPut in BulletScriptable yet", true)]
-        public void OnPut(CoordStruct coord, Direction faceDir)
-        {
-            throw new NotSupportedException("not support OnPut in BulletScriptable yet");
-        }
-        [Obsolete("not support OnRemove in BulletScriptable yet", true)]
-        public void OnRemove()
-        {
-            throw new NotSupportedException("not support OnRemove in BulletScriptable yet");
-        }
-        [Obsolete("not support OnReceiveDamage in BulletScriptable yet", true)]
-        public void OnReceiveDamage(Pointer<int> pDamage, int DistanceFromEpicenter, Pointer<WarheadTypeClass> pWH,
-            Pointer<ObjectClass> pAttacker, bool IgnoreDefenses, bool PreventPassengerEscape, Pointer<HouseClass> pAttackingHouse)
-        {
-            throw new NotSupportedException("not support OnReceiveDamage in BulletScriptable yet");
-        }
-
-        public virtual void OnDetonate(Pointer<CoordStruct> pCoords) { }
-    }
-
-    [Serializable]
-    public class SuperWeaponScriptable : Scriptable<SuperWeaponExt>, ISuperWeaponScriptable
-    {
-        public SuperWeaponScriptable(SuperWeaponExt owner) : base(owner)
-        {
-        }
-
-        public sealed override void OnUpdate()
-        {
-            throw new NotSupportedException("not support OnUpdate in SuperWeaponScriptable yet");
-        }
-        public sealed override void OnLateUpdate()
-        {
-            throw new NotSupportedException("not support OnLateUpdate in SuperWeaponScriptable yet");
-        }
-        public sealed override void OnRender()
-        {
-            throw new NotSupportedException("not support OnRender in SuperWeaponScriptable yet");
-        }
-
-        public virtual void OnLaunch(CellStruct cell, bool isPlayer) { }
-    }
-
-#if USE_ANIM_EXT
-    [Serializable]
-    public class AnimScriptable : Scriptable<AnimExt>, IAnimScriptable
-    {
-        public AnimScriptable(AnimExt owner) : base(owner)
-        {
-        }
-        
-        [Obsolete("not support OnPut in AnimScriptable yet", true)]
-        public void OnPut(CoordStruct coord, Direction faceDir)
-        {
-            throw new NotSupportedException("not support OnPut in AnimScriptable yet");
-        }
-        public virtual void OnRemove() { }
-        [Obsolete("not support OnReceiveDamage in AnimScriptable yet", true)]
-        public void OnReceiveDamage(Pointer<int> pDamage, int DistanceFromEpicenter, Pointer<WarheadTypeClass> pWH,
-            Pointer<ObjectClass> pAttacker, bool IgnoreDefenses, bool PreventPassengerEscape, Pointer<HouseClass> pAttackingHouse)
-        {
-            throw new NotSupportedException("not support OnReceiveDamage in AnimScriptable yet");
-        }
-    }
-#endif
 }
