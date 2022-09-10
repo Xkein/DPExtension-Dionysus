5b5d1437da06e0bad2a296bd02fb9dabcfc9c56a
Xkein <739502808@qq.com>
2022-09-11 02:22:04 +0800

    add BasicStructureParser

--- Projects/Extension/INI/YRParsers.cs
+++ Projects/Extension/INI/YRParsers.cs
@@ -1,9 +1,20 @@
 ﻿
 
+using Extension.Utilities;
+using PatcherYRpp;
+
 namespace Extension.INI
 {
     internal partial class YRParsers
     {
-        public static partial void Register();
+        public static void Register()
+        {
+            RegisterFindTypeParsers();
+
+            RegisterBasicStructureParsers();
+        }
+
+        public static partial void RegisterFindTypeParsers();
+        public static partial void RegisterBasicStructureParsers();
     }
 }
--- Projects/Extension/INI/YRParsers.g.cs
+++ Projects/Extension/INI/YRParsers.g.cs
@@ -6,14 +6,31 @@ namespace Extension.INI
 {
     internal partial class YRParsers
     {
-        public static partial void Register()
+        public static partial void RegisterFindTypeParsers()
         {
+            new BulletTypeClassParser().Register();
+            new HouseTypeClassParser().Register();
             new SuperWeaponTypeClassParser().Register();
             new TechnoTypeClassParser().Register();
             new WeaponTypeClassParser().Register();
+            new WarheadTypeClassParser().Register();
         }
     }
     
+    public class BulletTypeClassParser : FindTypeParser<BulletTypeClass>
+    {
+        public BulletTypeClassParser() : base(BulletTypeClass.ABSTRACTTYPE_ARRAY)
+        {
+
+        }
+    }
+    public class HouseTypeClassParser : FindTypeParser<HouseTypeClass>
+    {
+        public HouseTypeClassParser() : base(HouseTypeClass.ABSTRACTTYPE_ARRAY)
+        {
+
+        }
+    }
     public class SuperWeaponTypeClassParser : FindTypeParser<SuperWeaponTypeClass>
     {
         public SuperWeaponTypeClassParser() : base(SuperWeaponTypeClass.ABSTRACTTYPE_ARRAY)
@@ -31,6 +48,13 @@ namespace Extension.INI
     public class WeaponTypeClassParser : FindTypeParser<WeaponTypeClass>
     {
         public WeaponTypeClassParser() : base(WeaponTypeClass.ABSTRACTTYPE_ARRAY)
+        {
+
+        }
+    }
+    public class WarheadTypeClassParser : FindTypeParser<WarheadTypeClass>
+    {
+        public WarheadTypeClassParser() : base(WarheadTypeClass.ABSTRACTTYPE_ARRAY)
         {
 
         }
